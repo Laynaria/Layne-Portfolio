fcb684d6fff09fba79bb2c85e24d0ab26a2b7907
Laynaria <pro.laynaria@outlook.fr>
2022-12-08 11:56:52 +0100

    Burger menu functionnalities are now done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,29 +5,62 @@ import cross from "../../assets/Cross.svg";
 import "./Header.scss";
 
 function Header() {
+  // Burger State and function to change icon when clicking
   const [isBurger, setIsBurger] = useState(false);
 
   const burgerHandler = () => {
     setIsBurger(!isBurger);
   };
 
+  // ScrolltoTop Function when clicking on Logo
+  const scrollToTop = () => {
+    const body = document.querySelector("body");
+    body.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToProjects = () => {
+    const projectSection = document.querySelector("#projectsSection");
+    projectSection.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   return (
     <header>
-      <div id="burger">
-        {isBurger ? (
-          <img
-            src={cross}
-            alt="Burger Menu Icon Open"
-            onClick={burgerHandler}
-          />
-        ) : (
-          <img
-            src={burger}
-            alt="Burger Menu Icon Closed"
-            onClick={burgerHandler}
-          />
-        )}
-      </div>
+      {/* Menu Logo */}
+      <span id="logoMenu" onClick={scrollToTop}>
+        H
+      </span>
+
+      {/* Burger Menu Icon */}
+      {isBurger ? (
+        <img
+          className="burger"
+          src={cross}
+          alt="Burger Menu Icon Open"
+          onClick={burgerHandler}
+        />
+      ) : (
+        <img
+          className="burger"
+          src={burger}
+          alt="Burger Menu Icon Closed"
+          onClick={burgerHandler}
+        />
+      )}
+
+      {/* Navigation */}
+      {isBurger ? (
+        <nav>
+          <ul>
+            <li onClick={scrollToProjects}>Projects</li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
     </header>
   );
 }
