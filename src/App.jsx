2c703514d8ec88310ea074832dd83a39f2c0b8c5
Laynaria <pro.laynaria@outlook.fr>
2022-12-30 10:13:03 +0100

    added stars background component and css

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./components/Header/Header";
 import Welcome from "./components/Welcome/Welcome";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer.jsx";
+import BackgroundAnimation from "./components/BackgroundAnimation/BackgroundAnimation";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <BackgroundAnimation />
       <Header />
       <Welcome />
       <Projects />
---
+++ src/components/BackgroundAnimation/BackgroundAnimation.jsx
@@ -0,0 +1,13 @@
+import "./BackgroundAnimation.scss";
+
+function BackgroundAnimation() {
+  return (
+    <div id="backgroundAnimation">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+}
+
+export default BackgroundAnimation;
