63f5c86c9b332c364e33158879667c44f98021aa
Laynaria <pro.laynaria@outlook.fr>
2023-02-16 18:31:16 +0100

    some basic part of About Me secion structure, a lot of things to do for this section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import experiences from "../../services/experiences";
 
 import "./About.scss";
 
@@ -6,6 +7,20 @@ function About({ language }) {
   return (
     <section id="aboutSection">
       <h1>{language === "fr" ? "À propos" : "About Me"}</h1>
+
+      <div id="aboutBox">
+        {language === "fr" ? (
+          <p>Auteur et entrepreneur depuis 2017, </p>
+        ) : (
+          <p>Author and entrepreneur since 2017, </p>
+        )}
+        {experiences.map((experience) => (
+          <a href={experience.url} alt={`${experience.name} link`}>
+            <img src={experience.img} alt={`${experience.name} logo`} />
+            {experience.name}
+          </a>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/services/experiences.jsx
@@ -0,0 +1,23 @@
+import fbImg from "../assets/socialNetworks/Facebook.svg";
+import twitImg from "../assets/socialNetworks/Twitter.svg";
+import instaImg from "../assets/socialNetworks/Instagram.svg";
+
+const experiences = [
+  {
+    name: "Wild Code School",
+    img: fbImg,
+    url: "https://www.wildcodeschool.com/",
+  },
+  {
+    name: "Laynaria",
+    img: twitImg,
+    url: "https://laynaria.com/",
+  },
+  {
+    name: "TheARTchemists",
+    img: instaImg,
+    url: "https://www.theartchemists.com/",
+  },
+];
+
+export default experiences;
