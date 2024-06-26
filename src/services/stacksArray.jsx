b4d7c195a431b430594e3ab47c2a643e131e61f0
Laynaria <pro.laynaria@outlook.fr>
2024-06-26 19:46:17 +0200

    updated stack front and some more minor scss change

--- src/services/stacksArray.jsx
+++ src/services/stacksArray.jsx
@@ -1,7 +1,13 @@
-import reactLogo from "../assets/tools/react.svg";
 import javascriptLogo from "../assets/tools/javascript.svg";
-import cssLogo from "../assets/tools/css.svg";
+import typescriptLogo from "../assets/tools/typescript.svg";
+import reactLogo from "../assets/tools/react.svg";
+import nextLogo from "../assets/tools/next.svg";
+import vueLogo from "../assets/tools/vue.svg";
+import nuxtLogo from "../assets/tools/nuxt.svg";
 import htmlLogo from "../assets/tools/html.svg";
+import cssLogo from "../assets/tools/css.svg";
+import sassLogo from "../assets/tools/sass.svg";
+import tailwindLogo from "../assets/tools/tailwind.svg";
 
 import nodeLogo from "../assets/tools/node.svg";
 import mysqlLogo from "../assets/tools/mysql.svg";
@@ -25,14 +31,16 @@ const stacksArray = [
     title: "Frontend",
     titleEN: "Frontend",
     languages: [
-      {
-        id: 0,
-        name: "React",
-        img: reactLogo,
-      },
-      { id: 1, name: "Javascript", img: javascriptLogo },
-      { id: 2, name: "CSS3", img: cssLogo },
-      { id: 3, name: "HTML5", img: htmlLogo },
+      { id: 0, name: "Javascript", img: javascriptLogo },
+      { id: 1, name: "Typescript", img: typescriptLogo },
+      { id: 2, name: "React", img: reactLogo },
+      { id: 3, name: "Next Js", img: nextLogo },
+      { id: 4, name: "Vue.Js", img: vueLogo },
+      { id: 5, name: "Nuxt Js", img: nuxtLogo },
+      { id: 6, name: "HTML5", img: htmlLogo },
+      { id: 7, name: "CSS3", img: cssLogo },
+      { id: 8, name: "SASS", img: sassLogo },
+      { id: 9, name: "Tailwind", img: tailwindLogo },
     ],
   },
   {
